88da87fdd563673df472b9e751093a7fa512edad
bharata <bharata@bharata-desktop>
2017-04-10 19:00:18 +0900

    extract for ensembl id assignment

--- src/transfacparser/MainClass.java
+++ src/transfacparser/MainClass.java
@@ -14,10 +14,23 @@ public class MainClass {
         String fileDB = "resources/mart_export.csv";
         String output = "resources/geneListEnsembl.csv";
         TransfacFile file = TransfacParser.parseTransfacData(fileName);
-        Map geneDB = EnsemblGeneDB.ReadGeneDB(fileDB);
+        Map<String,String> geneDB = EnsemblGeneDB.ReadGeneDB(fileDB);
         
-        PrintWriter pw = new PrintWriter(new File(output));
+        
+        
+        for(Object obj:file.getMatrixID()){
+            TransfacMatrix mtrx = file.getMatrix((String)obj);
+            if(!mtrx.isHumanGene())
+                continue;
+            if(geneDB.containsKey(mtrx.getTFID())){
+                geneDB.put(mtrx.getGeneName().toUpperCase(), geneDB.get(mtrx.getTFID()));
+            }
+        }
+        
         Set<String> allTFID = new HashSet<>();
+        PrintWriter pw = new PrintWriter(new File(output));
+        pw.write("TFID\tGeneName\tEnsemblID\n");
+        Set<String> notFound = new HashSet<>();
         for(Object obj:file.getMatrixID()){
             TransfacMatrix mtrx = file.getMatrix((String)obj);
             if(!mtrx.isHumanGene())
@@ -27,12 +40,38 @@ public class MainClass {
             allTFID.add(mtrx.getTFID());
             String s = mtrx.getTFID()+"\t"+mtrx.getGeneName();
             if(geneDB.containsKey(mtrx.getTFID())){
-                s+="\t"+geneDB.get(mtrx.getTFID()).toString()+"\n";
+                s+="\t"+geneDB.get(mtrx.getTFID())+"\n";
+                geneDB.put(mtrx.getGeneName().toUpperCase(), geneDB.get(mtrx.getTFID()));
             }else{
+                
+                String[] parsed = mtrx.getGeneName().split(":");
+                if(parsed.length>0){
+                    s+="\t";
+                    for(String xx:parsed){
+                        xx = xx.toUpperCase();
+                        
+                        if(geneDB.containsKey(xx)){
+                            s+=geneDB.get(xx);
+                        }else{
+                            notFound.add(xx);
+                            s+="NA";
+                        }
+                        s+=":";
+                    }
+                }
+                if(s.charAt(s.length()-1)==':'){
+                    s = s.substring(0, s.length()-1);
+                }
                 s+="\n";
             }
             pw.write(s);
-        }
+        }    
+
         pw.close();
+        int n=1;
+        for(String notfound:notFound){
+            System.out.println(n+"\t"+notfound);
+            n++;
+        }
     }
 }
